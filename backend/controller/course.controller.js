48cdf61050424a50d0a29852625e9b4d9ae1362e
Danish-Devx3 <danish.devx3@gmail.com>
2025-04-02 00:55:01 +0530

    Enhance course playlist functionality; add getPlaylists route and improve error handling

--- backend/controller/course.controller.js
+++ backend/controller/course.controller.js
@@ -1,21 +1,35 @@
 import { User } from "../db.js";
 import jwt from "jsonwebtoken";
 
-export const addplaylist = async (req,res) => {
-    const playlist = req.body.playlist;
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    try {
-        const user = await User.findById(decodedToken.user_id);
-        if (!user) return res.status(404).json({ message: 'User not found' });
-        if (user.playlists.includes(playlistId)) {
-            return res.status(400).json({ message: 'Playlist already exists' });
-        }
-        user.playlists.push(playlistId);
-        await user.save();
-        return res.json(user);
-        } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+export const addplaylist = async (req, res) => {
+  const playlistLink = req.body.playlist;
+  const playlistId = playlistLink.split("/")[playlistLink.split("/").length - 1];
+  const token = req.headers.authorization.split(" ")[1];
+  const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    const user = await User.findById(decodedToken.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    if (user.playlists.includes(playlistId)) {
+      return res.status(400).json({ message: "Playlist already exists" });
     }
-}
+    user.playlists.push(playlistId);
+    await user.save();
+    return res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+export const getPlaylists = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const user = await User.findById(userId);
+        if (!user) return res.status(404).json({ message: "User not found" });
+        return res.json(user.playlists);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+};
--- backend/routes/courseRoute.js
+++ backend/routes/courseRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { addplaylist } from '../controller/course.controller.js';
+import { addplaylist, getPlaylists } from '../controller/course.controller.js';
 import userMiddleware from '../middleware/userMiddleware.js';
 
 const Router = express.Router();
 
 Router.post('/add', addplaylist );
+
+Router.get('/playlists', userMiddleware, getPlaylists )
 
 
 
